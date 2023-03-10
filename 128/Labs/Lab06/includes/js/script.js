403a4cd53d0eeaebe75ea70dd3ad4800f9e7cb3b
gage <C0517025@online.camosun.ca>
2023-03-10 15:10:34 -0800

    Lab 06 FINISHED

--- 128/Labs/Lab06/includes/js/script.js
+++ 128/Labs/Lab06/includes/js/script.js
@@ -1,10 +1,8 @@
-
-
-
+//hotel class
 class Hotel {
 	//class constructor
     constructor(name, city, rooms, numRooms, maxRooms, gym, restaurants, shuttle, swimmingPool) {
-        this.name = name;
+    	this.name = name;
         this.city = city;
         this.rooms = rooms;
         this.numRooms = numRooms;
@@ -72,101 +70,205 @@ class Hotel {
 	}
 }
 
+//resort class
 class Resort extends Hotel {
-	
+	constructor(name, city, rooms, numRooms, maxRooms, gym, restaurants, shuttle, swimmingPool, resortType, beachFront, kidsClub, bar) {
+		super(name, city, rooms, numRooms, maxRooms, gym, restaurants, shuttle, swimmingPool);
+		this.resortType = resortType;
+		this.beachFront = beachFront;
+		this.kidsClub = kidsClub;
+		this.bar = bar;
+	}
+	//setters
+	set resort(resortType) {
+		this.resortType = resortType;
+	}
+	set isBeach(beachFront) {
+		this.beachFront = beachFront;
+	}
+	set hasKidsClub(kidsClub) {
+		this.kidsClub = kidsClub;
+	}
+	set hasBar(bar) {
+		this.bar = bar;
+	}
+	//getters
+	get resort() {
+		return this.resortType;
+	}
+	get isBeach() {
+		return this.beachFront;
+	}
+	get hasKidsClub() {
+		return this.kidsClub;
+	}
+	get hasBar() {
+		return this.bar;
+	}
 }
 //some variables
-
-
-
-//let poloniaHotel = new Resort(`Hotel Polonia`,`Warsaw`, poloniaRooms, 6, 9, )
+let hotelName = document.getElementById(`hotelName`);
+let hotelDescName = document.getElementById(`hotelDescName`);
+let hotelCity = document.getElementById(`hotelCity`);
+let hotelRooms = document.getElementById(`hotelRooms`);
+let gymService = document.getElementById(`gymService`);
+let shuttleService = document.getElementById(`shuttleService`);
+let restaurant = document.getElementById(`restaurant`);
+let hotelRestaurants = document.getElementById(`hotelRestaurants`);
+let hotelRoomsAvailable = document.getElementById(`hotelRoomsAvailable`);
+let fullyBooked = document.getElementById(`fullyBooked`);
+let isBeachFront = document.getElementById(`beachFront`);
+let barService = document.getElementById(`barService`);
+let newBar = document.getElementById(`barConstruction`);
+let barConstruction = true;
 const savoyRooms = ["Single", " Double", " Suite"];
-const restaurants1 = [["Donde La Arepa", "Colombian"], 
-                    ["Casa Ramen", "Japanese"], 
-                    ["Pizza Hermosa", "Italian"]]; 
-let savoyHotel = new Hotel(`Savoy Hotel`, `London`, savoyRooms, 25, 100, true, restaurants1, true, true);
-
-
-let savoyModalText = `
-  <div class="modal fade" id="savoyModal" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title display-4">${savoyHotel.hotelName}</h1>
-          <button id="savoyModalClose" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-        <a href="#" class="text-lead" style="text-decoration:none; color:white;">Hotel Info</a>
-        <p class="text-lead mt-2">The <span class="fw-bold">${savoyHotel.hotelName}</span> is located in ${savoyHotel.cityName}.</p>
-        <p class="text-lead mt-4">The Available types of rooms are: ${savoyHotel.roomType}.</p>
-        <hr>
-        <h1 class="display-6">Services</h1>
-        <span class="fw-bold">Shuttle Service - </span><span> ${savoyHotel.hasShuttle}</span><br>
-        <span class="fw-bold mt-2">Gym - </span><span> ${savoyHotel.hasPool}</span><br>
-		<p class="fw-bold mt-2">Hotel has 3 restaurants each with a different theme:</p>
-		`;
-
-		for (let [key, value] of restaurants1) {
-			savoyModalText += `<span class="fw-bold">${key}</span><span> / Type / </span><span class="fw-bold">${value}</span><br>`
-		}
-
-		savoyModalText += `
-		<span id="savoyRoomsAvailable" style="color: green;">Hotel has ${savoyHotel.roomAmt}/${savoyHotel.totalRooms} rooms booked.</span><br>
-		<span id="fullyBookedSavoy"></span>
-        </div>
-        <div class="modal-footer">
-          <button type="button" id="savoyBookBtn" class="btn btn-primary">Book Room</button>
-          <button type="button" id="savoyCancelBtn" class="btn btn-danger">Cancel Room</button>
-        </div>
-      </div>
-    </div>
-  </div>
-`;
-
-
+const savoyRestaurants = 	[["Donde La Arepa", "Colombian"], 
+                    		["Casa Ramen", "Japanese"], 
+                    		["Pizza Hermosa", "Italian"]]; 
+const poloniaRooms = ["King", " Suite", " Penthouse"];
+const poloniaRestaurants = 	[["Bistro 94", "French"],
+							["Casa Bonita", "Mexican"],
+							["Pizza Si", "Italian"]];
+const modal1 = document.getElementById(`hotelModal`);
+const hotelModalBS = new bootstrap.Modal(modal1);
 const savoyBtn = document.getElementById(`savoyBtn`);
-const modal1 = document.getElementById(`savoyModal`);
-const savoyBookBtn = document.getElementById(`savoyBookBtn`);
-const savoyCancelBtn = document.getElementById(`savoyCancelBtn`);
-const savoyCloseBtn = document.getElementById(`savoyModalClose`);
+const poloniaBtn = document.getElementById(`poloniaBtn`);
+const hotelBookBtn = document.getElementById(`hotelBookBtn`);
+const hotelCancelBtn = document.getElementById(`hotelCancelBtn`);
+const hotelModalClose = document.getElementById(`hotelModalClose`);
+//class instances
+let savoyHotel = new Hotel(`Savoy Hotel`, `London`, savoyRooms, 4, 20, true, savoyRestaurants, true, true);
+let poloniaHotel = new Resort(`Hotel Polonia`, `Warsaw`, poloniaRooms, 6, 9, true, poloniaRestaurants, true, true, `Resort`, false, true, false);
 
+//close modal
+hotelModalClose.addEventListener(`click`, function () {
+	//remove button event listeners on close
+	hotelBookBtn.removeEventListener(`click`, poloniaBookBtn);
+	hotelCancelBtn.removeEventListener(`click`, poloniaCancelBtn);
+	hotelBookBtn.removeEventListener(`click`, savoyBookBtn);
+	hotelCancelBtn.removeEventListener(`click`, savoyCancelBtn);
+	hotelName.innerHTML = ``; //clears modal content on close
+	hotelDescName.innerHTML = ``;
+	hotelCity.innerHTML = ``;
+	hotelRooms.innerHTML = ``;
+	gymService.innerHTML = ``;
+	shuttleService.innerHTML = ``;
+	restaurant.innerHTML = ``;
+	hotelRestaurants.innerHTML = ``;
+	hotelRoomsAvailable.innerHTML = ``;
+	fullyBooked.innerHTML = ``;
+	isBeachFront.innerHTML = ``;
+	barService.innerHTML = ``;
+	newBar.innerHTML = ``;
+	hotelModalBS.hide();
+});
 
+//event handlers for cards
+savoyBtn.addEventListener('click', showSavoyModal);
+poloniaBtn.addEventListener(`click`, showPoloniaModal);
 
+//hotel polonia modal
+function showPoloniaModal() {
+	hotelModalBS.show();
+	hotelBookBtn.addEventListener(`click`, poloniaBookBtn);
+	hotelCancelBtn.addEventListener(`click`, poloniaCancelBtn);
+	hotelName.innerHTML = poloniaHotel.hotelName;
+	hotelDescName.innerHTML = poloniaHotel.hotelName;
+	hotelCity.innerHTML = poloniaHotel.cityName;
+	hotelRooms.innerHTML = poloniaHotel.roomType;
+	isBeachFront.innerHTML = `<span class="fw-bold">Beachfront - </span>${poloniaHotel.isBeach}<br>`;
+	barService.innerHTML = `<span class="fw-bold">Bar - </span>${poloniaHotel.hasBar}<br><br>`;
+	newBar.innerHTML = `<span class="fw-bold">NEWS! </span><span>There is a </span><span class="fw-bold">New Bar</span><span> being built!<br><br>
+	<span class="fw-bold">Bar service</span> - ${barConstruction}<br><br>`;
+	restaurant.innerHTML = `Hotel has 3 restaurants each with a different theme:`;
+	
+	for (let [key, value] of poloniaHotel.restaurantTypes) {
+		hotelRestaurants.innerHTML += `<span class="fw-bold">${key}</span> / Type / <span class="fw-bold">${value}<br>`;
+	}
 
-savoyBtn.addEventListener(`click`, function(e) {
-	document.getElementById(`savoyDisplay`).innerHTML = savoyModalText;
-	e.preventDefault();
-	const savoyModalBS = new bootstrap.Modal(modal1);
-	savoyModalBS._element.addEventListener(`shown.bs.modal`, function() {
-	  const savoyBookBtn = document.getElementById('savoyBookBtn');
-	  const savoyCancelBtn = document.getElementById('savoyCancelBtn');
-	  savoyBookBtn.addEventListener('click', bookBtn);
-	  savoyCancelBtn.addEventListener('click', cancelBtn);
-	  savoyModalBS.show();
-	});
-  });
+	hotelRoomsAvailable.innerHTML = `<br><span style="color: green;">Hotel has ${poloniaHotel.roomAmt}/${poloniaHotel.totalRooms} rooms booked.</span><br>`;
 
-function bookBtn() {
-	if (savoyHotel.roomAmt < savoyHotel.totalRooms) {
-		savoyHotel.roomAmt++;
-		document.getElementById(`savoyRoomsAvailable`).innerHTML = `<span style="color: green;">Hotel has ${savoyHotel.roomAmt}/${savoyHotel.totalRooms} rooms booked.</span>`;
-		if (savoyHotel.roomAmt === savoyHotel.totalRooms) {
-		  document.getElementById(`fullyBookedSavoy`).innerHTML = `<span style="color: red;">Hotel is fully booked!</span>`;
-		}
-	  } else {
-		document.getElementById(`fullyBookedSavoy`).innerHTML = `<span style="color: red;">Hotel is fully booked!</span>`;
-	  }
+	if (poloniaHotel.roomAmt === poloniaHotel.totalRooms) {
+		fullyBooked.innerHTML = `<span style="color: red;">Hotel is fully booked!</span>`;
+	}
 }
 
-function cancelBtn() {
+//savoy hotel modal
+function showSavoyModal() {
+	hotelModalBS.show();
+	hotelBookBtn.addEventListener(`click`, savoyBookBtn);
+	hotelCancelBtn.addEventListener(`click`, savoyCancelBtn);
+	hotelName.innerHTML = savoyHotel.hotelName;
+	hotelDescName.innerHTML = savoyHotel.hotelName;
+	hotelCity.innerHTML = savoyHotel.cityName;
+	hotelRooms.innerHTML = savoyHotel.roomType;
+	gymService.innerHTML = `<span class="fw-bold">Gym Service</span> - ${savoyHotel.hasGym}<br>`;
+	shuttleService.innerHTML = `<span class="fw-bold">Shuttle Service</span> - ${savoyHotel.hasShuttle}<br>`;
+	restaurant.innerHTML = `Hotel has 3 restaurants each with a different theme:`;
+
+	for (let [key, value] of savoyHotel.restaurantTypes) {
+		hotelRestaurants.innerHTML += `<span class="fw-bold">${key}</span> / Type / <span class="fw-bold">${value}<br>`;
+	}
+  
+	hotelRoomsAvailable.innerHTML = `<br><span style="color: green;">Hotel has ${savoyHotel.roomAmt}/${savoyHotel.totalRooms} rooms booked.</span><br>`;
+
+	if (savoyHotel.roomAmt === savoyHotel.totalRooms) {
+		fullyBooked.innerHTML = `<span style="color: red;">Hotel is fully booked!</span>`;
+	}
+}
+
+//functions for booking/cancelling rooms for both hotels
+function savoyBookBtn() {
+	if (savoyHotel.roomAmt < savoyHotel.totalRooms) {
+		savoyHotel.roomAmt++;
+		fullyBooked.innerHTML= ``;
+		hotelRoomsAvailable.innerHTML = `<br><span style="color: green;">Hotel has ${savoyHotel.roomAmt}/${savoyHotel.totalRooms} rooms booked.</span><br>`;
+		if (savoyHotel.roomAmt === savoyHotel.totalRooms) {
+			fullyBooked.innerHTML = `<span style="color: red;">Hotel is fully booked!</span>`;
+		}
+	} 
+	else {
+		fullyBooked.innerHTML = `<span style="color: red;">Hotel is fully booked!</span>`;
+	}
+}
+
+function savoyCancelBtn() {
 	if (savoyHotel.roomAmt === savoyHotel.totalRooms){
-		document.getElementById(`fullyBookedSavoy`).innerHTML = "";
-	  }
-	  if (savoyHotel.roomAmt === 0) {
-		document.getElementById(`savoyRoomsAvailable`).innerHTML = `<span style="color: green;">Hotel has ${savoyHotel.roomAmt}/${savoyHotel.totalRooms} rooms booked.</span>`;
-	  }
-	  else {
+		fullyBooked.innerHTML = "";
+	}
+	if (savoyHotel.roomAmt === 0) {
+		hotelRoomsAvailable.innerHTML = `<br><span style="color: green;">Hotel has ${savoyHotel.roomAmt}/${savoyHotel.totalRooms} rooms booked.</span><br>`;
+	}
+	else {
 		savoyHotel.roomAmt--;
-		document.getElementById(`savoyRoomsAvailable`).innerHTML = `<span style="color: green;">Hotel has ${savoyHotel.roomAmt}/${savoyHotel.totalRooms} rooms booked.</span>`;
-	  }
+		hotelRoomsAvailable.innerHTML = `<br><span style="color: green;">Hotel has ${savoyHotel.roomAmt}/${savoyHotel.totalRooms} rooms booked.</span><br>`;
+	}
+}
+
+
+function poloniaBookBtn() {
+	console.log(poloniaHotel.roomAmt);
+	if (poloniaHotel.roomAmt < poloniaHotel.totalRooms) {
+		poloniaHotel.roomAmt++;
+		hotelRoomsAvailable.innerHTML = `<br><span style="color: green;">Hotel has ${poloniaHotel.roomAmt}/${poloniaHotel.totalRooms} rooms booked.</span><br>`;
+		if (poloniaHotel.roomAmt === poloniaHotel.totalRooms) {
+			fullyBooked.innerHTML = `<span style="color: red;">Hotel is fully booked!</span>`;
+		}
+	} 
+	else {
+		fullyBooked.innerHTML = `<span style="color: red;">Hotel is fully booked!</span>`;
+	}
+}
+
+function poloniaCancelBtn() {
+	if (poloniaHotel.roomAmt === poloniaHotel.totalRooms){
+		fullyBooked.innerHTML = "";
+	}
+	if (poloniaHotel.roomAmt === 0) {
+		hotelRoomsAvailable.innerHTML = `<br><span style="color: green;">Hotel has ${poloniaHotel.roomAmt}/${poloniaHotel.totalRooms} rooms booked.</span><br>`;
+	}
+	else {
+		poloniaHotel.roomAmt--;
+		hotelRoomsAvailable.innerHTML = `<br><span style="color: green;">Hotel has ${poloniaHotel.roomAmt}/${poloniaHotel.totalRooms} rooms booked.</span><br>`;
+	}
 }
